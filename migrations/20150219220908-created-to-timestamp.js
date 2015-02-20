fba290d101c99f87a28f069644d795d237c7e2d0
Sean Brewer <seabre986@gmail.com>
2015-02-20 10:56:48 -0500

    Missing semicolons.

--- migrations/20150219220908-created-to-timestamp.js
+++ migrations/20150219220908-created-to-timestamp.js
@@ -2,22 +2,22 @@
 
 module.exports = {
   up: function(migration, DataTypes, done) {
-    migration.removeColumn('entries', 'created')
+    migration.removeColumn('entries', 'created');
     migration.addColumn(
       'entries',
       'created',
       DataTypes.DATE
-    )
+    );
     done();
   },
 
   down: function(migration, DataTypes, done) {
-    migration.removeColumn('entries', 'created')
+    migration.removeColumn('entries', 'created');
     migration.addColumn(
       'entries',
       'created',
       DataTypes.STRING
-    )
+    );
     done();
   }
 };
